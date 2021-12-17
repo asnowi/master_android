3550bc94aea72ca7789e424256326184614ec1d7
gaoguanqi <307590625@qq.com>
2021-12-18 02:58:27 +0800

    build.gradle

--- buildSrc/src/main/java/com/maple/buildsrc/ProjectBuildConfig.kt
+++ buildSrc/src/main/java/com/maple/buildsrc/ProjectBuildConfig.kt
@@ -6,19 +6,26 @@ package com.maple.buildsrc
 object ProjectBuildConfig {
 
     const val applicationId = "com.maple.master"
-
     const val compileSdkVersion = 31
     const val buildToolsVersion = "31.0.0"
     const val minSdkVersion = 21
     const val targetSdkVersion = 31
-
-    const val versionLibCode = 100
-    const val versionLibName = "1.0.0"
-
     const val versionCode = 100
     const val versionName = "1.0.0"
-
+    const val versionLibCode = 100
+    const val versionLibName = "1.0.0"
     const val isAppMode = false
+
+
+    /// smart
+    const val smartApplicationId = "com.maple.master.smart"
+    const val smartVersionCode = 100
+    const val smartVersionName = "1.0.0"
+
+    /// test
+    const val testApplicationId = "com.maple.master.test"
+    const val testVersionCode = 100
+    const val testVersionName = "1.0.0"
 
 
     /**
